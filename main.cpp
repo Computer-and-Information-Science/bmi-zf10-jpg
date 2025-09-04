1d1ad9d72e973107646608274de9fe0a3f567fcc
zf10-jpg <zahira.fernandez@student.northampton.edu>
2025-09-04 21:20:11 +0000

    first coding run in finding BMI of 24.56 found 24.563

--- main.cpp
+++ main.cpp
@@ -2,7 +2,23 @@
 using namespace std;
 
 int main() {
+    const int height = 61;
+    const int weight =130;
+
+    double kg;
+    double meters;
+    
+    //calculations
+    kg = weight * 0.453592;
+    meters = height * 0.0254;
+
+    //calculate bmi
+
+    double bmi;
+    bmi = kg/(meters * meters);
  
+
+    cout <<"The BMI for this person is :"<< bmi<< endl;
 
 
 }
